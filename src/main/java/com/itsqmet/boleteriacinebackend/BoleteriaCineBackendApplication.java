d2a0186785d21659d0accdadc343a5a2027c3df7
kevledex <kevinledesma014@gmail.com>
2026-09-13 15:02:12 -0500

    Inicio de backend

---
+++ src/main/java/com/itsqmet/boleteriacinebackend/BoleteriaCineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.itsqmet.boleteriacinebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoleteriaCineBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoleteriaCineBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itsqmet/boleteriacinebackend/BoleteriaCineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsqmet.boleteriacinebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoleteriaCineBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
